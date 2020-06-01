3ac84fe9f9735f57cabc3b0c5e65b03715597441
korrakot1396 <freetoplay.love@hotmail.com>
2020-06-01 09:44:55 +0700

    Updates

---
+++ precache-manifest.d1db756e8b9b0ec17b645aadeb1e93fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd85c9030697617f19ccb9f4b7353ab",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "b440c5a713f1d1374227",
+    "url": "/resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca4f6208a3fc795cbbd2",
+    "url": "/resume/static/js/2.e3883bac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.e3883bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b440c5a713f1d1374227",
+    "url": "/resume/static/js/main.97f6fb2e.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
