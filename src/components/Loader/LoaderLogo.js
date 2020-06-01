c9c8e98878023a96269abcde39d7a10c84626ed5
korrakot1396 <freetoplay.love@hotmail.com>
2020-06-01 10:02:02 +0700

    update

---
+++ src/components/Loader/LoaderLogo.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./LoaderLogo.css";
+
+class LogoLoader extends React.Component {
+  render() {
+    return (
+   
+  <div class="center">
+  <div class="fakeMenu">
+  <div class="fakeButtons fakeClose"></div>
+  <div class="fakeButtons fakeMinimize"></div>
+  <div class="fakeButtons fakeZoom"></div>
+  </div>
+ 
+
+<div class="fakeScreen">
+  <p class="line1">&#91;&nbsp;&ldquo;I'm a Korrakot.&rdquo;,<span class="cursor1">_</span></p>
+  <p class="line2">&nbsp;&nbsp;&ldquo;I'm a Web Developer.&rdquo;,<span class="cursor2">_</span></p>
+  <p class="line3">&nbsp;&nbsp;&ldquo;Welcome My Website.&rdquo;&nbsp;&#93;<span class="cursor2">_</span></p>
+  <p class="line4">><span class="cursor4">_</span></p>
+</div>
+</div>
+
+
+  
+
+
+    );
+  }
+}
+
+export default LogoLoader;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BaseProvider, LightTheme } from "baseui";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+
+import "./index.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./assests/font-awesome/css/all.css";
+
+const engine = new Styletron();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<StyletronProvider value={engine}>
+		<BaseProvider theme={LightTheme}>
+				<App />
+	</BaseProvider>
+	</StyletronProvider>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
