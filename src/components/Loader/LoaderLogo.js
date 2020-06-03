60e00cd68ef64e4ca456cce73ad90bfaa43da989
korrakot1396 <freetoplay.love@hotmail.com>
2020-06-03 09:27:12 +0700

    update resume

--- src/components/Loader/LoaderLogo.js
+++ src/components/Loader/LoaderLogo.js
@@ -14,7 +14,7 @@ class LogoLoader extends React.Component {
  
 
 <div class="fakeScreen">
-  <p class="line1">&#91;&nbsp;&ldquo;I'm Korrakot.&rdquo;,<span class="cursor1">_</span></p>
+  <p class="line1">&#91;&nbsp;&ldquo;I'm Korrakot Triwichian.&rdquo;,<span class="cursor1">_</span></p>
   <p class="line2">&nbsp;&nbsp;&ldquo;I'm a Software Developer.&rdquo;,<span class="cursor2">_</span></p>
   <p class="line3">&nbsp;&nbsp;&ldquo;Welcome My Website.&rdquo;&nbsp;&#93;<span class="cursor2">_</span></p>
   <p class="line4">><span class="cursor4">_</span></p>
