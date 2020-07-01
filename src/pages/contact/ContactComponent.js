40d3857eb1feef2efe535647f095981e6aaf2086
korrakot1396 <korrakot.tr@ku.th>
2020-07-01 14:03:26 +0700

    update resume

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -14,7 +14,7 @@ const blogSection = {
 	title: "Blogs",
 	subtitle:
 		"For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-	link: "https://ashutoshhathidara.wordpress.com",
+	link: "",
 	avatar_image_path: "blogs_image.svg"
 };
 
