baa12a1274950bff92f9c9edc5fb45b4d761f620
korrakot <korrakot.tr@gmail.com>
2021-08-26 09:20:58 +0700

    update resume

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
 import Main from "./containers/Main";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div>
+       <Helmet>
+                <meta charSet="utf-8" />
+                <title>Korrakot Resume</title>
+                <link rel="canonical" href="https://raw.githubusercontent.com/korrakot1396/resume/master/public/favicon.png" />
+            </Helmet>
       <Main />
     </div>
   );
