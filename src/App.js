93aa752dda77ecd2ad67e6be31df0bcb790c8172
korrakot <korrakot.tr@gmail.com>
2021-08-26 10:13:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
        <Helmet>
                 <meta charSet="utf-8" />
                 <title>Korrakot's Portfolio</title>
-                <link rel="canonical" href="https://raw.githubusercontent.com/korrakot1396/resume/master/public/favicon.png" />
+                <link rel="canonical" href="https://logobly.com/wp-content/uploads/001-rocket-1.png" />
             </Helmet>
       <Main />
     </div>
