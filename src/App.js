b9ba5a88fe967beb260a7db4480505953f109c1a
korrakot <korrakot.tr@gmail.com>
2021-08-27 08:59:49 +0700

    update resume

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import {Helmet} from "react-helmet";
 function App() {
   return (
     <div>
-       <Helmet>
-                <meta charSet="utf-8" />
-                <title>Korrakot's Portfolio</title>
-                <link rel="canonical" href="https://drive.google.com/file/d/1Exso7XAlIcOIpJWvfLU8UaCbbB6rwqla/view?usp=sharing" />
-            </Helmet>
       <Main />
     </div>
   );
