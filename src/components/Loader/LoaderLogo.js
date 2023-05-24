1a4013de83cf0f9f3df54dbdae388f26decb1796
korrakot.tr <korrakot@azurecomputer.com>
2023-05-24 15:03:33 +0700

    updated

--- src/components/Loader/LoaderLogo.js
+++ src/components/Loader/LoaderLogo.js
@@ -1,15 +1,33 @@
 import React from "react";
 import "./LoaderLogo.css";
-import GifLoader from 'react-gif-loader';
+// import GifLoader from 'react-gif-loader';
+import Lottie from 'react-lottie';
+import animationData from './LoaderLogo.json';
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
 
 const LogoLoader = () => {
   return (
-    <GifLoader
-      loading={true}
-      imageSrc="https://drive.google.com/uc?id=1GXXFtHJMXRLXlmFV0nyYY51Jvpyxo7ls"
-      imageStyle={{ width: "100%", height: "140%" }}
-      overlayBackground="rgba(0,0,0,0.5)"
-    />
+    // <GifLoader
+    //   loading={true}
+    //   imageSrc="https://drive.google.com/uc?id=1GXXFtHJMXRLXlmFV0nyYY51Jvpyxo7ls"
+    //   imageStyle={{ width: "100%", height: "140%" }}
+    //   overlayBackground="rgba(0,0,0,0.5)"
+    // />
+    <div className="lottie-container">
+  <Lottie 
+    options={defaultOptions}
+    height="100%"
+    width="100%"
+  />
+</div>
   );
 };
 
